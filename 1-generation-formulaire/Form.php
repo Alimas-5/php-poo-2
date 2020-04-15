e4abd57b69fbb189019ef2d15d03291537058f87
unknown <ludovic.bourget@live.fr>
2020-04-15 16:06:45 +0200

    rendu 2

--- 1-generation-formulaire/Form.php
+++ 1-generation-formulaire/Form.php
@@ -1,4 +1,61 @@
 <?php
 class Form{
-    
+    public $action;
+    public $method;
+    private $fields=[];
+    private $button;
+
+
+    public function __construct(string $action, string $method) {
+        $this->action = $action;
+        $this->method = $method;
+    }
+// cette fonction permet d'ajouter une case permetant d'avoir du texte 
+    public function Textadd (string $name, string $value) {
+        $this->fields[]= "<input type='text' name='$name' value='$value'>";
+        return $this;
+    }
+// cette fonction permet d'ajouter une case permetant d'avoir des nombre
+    public function Numberadd (string $name, int $value) {
+        $this->fields[] = "<input type='number' name='$name' value='$value'>";
+        return $this;
+    }
+// cette fonction permet d'avoir une case a valider pour de futur mise a jour
+    public function Checkboxadd(String $name, bool $value) {
+        $checked = ($value)?'checked':'';
+        $this->fields[] = "<input type='checkbox' name='$name' $checked>";
+        return $this;
+    }
+// cette fonction permet d'ajouter un bouton d'envoie
+    public function addSubmitButton($buttonname)
+    {
+        $this->fields[] = "<input type='submit' value='$buttonname'>";
+        return $this;
+    }
+// cette fonction permet d'ajouter une barre de séléction
+// cette fonction Selectadd n'est pas terminé
+    public function Selectadd(string $options,string $name,int $value) {
+        $this->fields[] = "
+        <select name='$name'>
+        <option value='$value'>Choisir</option>
+            <option value='1'>Rennes</option>
+            <option value='2'>Paris</option>
+            <option value='3'>Rouen</option>
+            <option value='4'>Caen</option>
+        </select>";
+        return $this;
+    }
+// cette fonction permet de fermer le fomulaire avec la balise HTML
+
+    public function build()
+    {
+        $html = "<form action='$this->action' method='$this->method'>";
+        foreach($this->fields as $field){
+            $html .= $field;
+        }
+        $html .= $this->button;
+        $html .= '</form>';
+        return $html;
+    }
+
 }
--- 1-generation-formulaire/index.php
+++ 1-generation-formulaire/index.php
@@ -2,12 +2,18 @@
 include 'Form.php';
 
 // Des variables par défaut pour vos tests.
-
+$action = '#';
+$method = 'POST';
+$nom = 'Ruo';
+$prenom = 'Bily';
 // YOUR CODE HERE
 
-$form = new Form($action, $method);  // créer le début du formulaire
-$form->addTextField('nom',$nom); // créer un input de type texte avec comme valeur par défaut $nom
-$form->addTextField('prenom',$prenom); // créer un input de type texte avec comme valeur par défaut $prenom
-$form->addSubmitButton('Modifier'); //Créer un bouton pour soumettre le formulaire se nommant Modifier
+$form = new Form($action, $method);  // création du formulaire
+
+$form->Textadd('nom',$nom); //input de type texte avec comme valeur par défaut $nom
+$form->Textadd('prenom',$prenom); //input de type texte avec comme valeur par défaut $prenom
+$form->Numberadd('Age',15);
+$form->addSubmitButton('Send'); //bouton pour soumettre le formulaire se nommant send
+$form->Selectadd('1','title',1); //test
 
 echo $form->build(); // générer le formulaire
